93d888e8511ab5a6577257a8b3d13722baa9b692
Vishal Thakur <vishalrocks158@gmail.com>
2026-07-10 13:34:06 +0530

    Fixed security.tf Checkov findings

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -35,12 +35,13 @@ resource "aws_security_group" "devops_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
+egress {
+    description = "Allow outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+}
 
   tags = {
     Name = "DevOps-SG"
