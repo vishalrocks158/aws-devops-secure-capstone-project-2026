9e7eca05c43d7f6a430d06b0098cae061efdecd3
Vishal Thakur <vishalrocks158@gmail.com>
2026-07-10 13:28:40 +0530

    Fixed EC2 Checkov findings

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -7,6 +7,24 @@ resource "aws_instance" "devops_server" {
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   associate_public_ip_address = true
 
+  # Enable detailed CloudWatch monitoring
+  monitoring = true
+
+  # Enable EBS optimization
+  ebs_optimized = true
+
+  # Enforce Instance Metadata Service Version 2 (IMDSv2)
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  # Encrypt root EBS volume
+  root_block_device {
+    encrypted = true
+    volume_type = "gp3"
+  }
+
   tags = {
     Name        = "DevOps-Server"
     Environment = "Development"
